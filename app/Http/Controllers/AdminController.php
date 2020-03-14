8e2fa9e489ec5e983142a73e0a3c3a870b93865e
Mr.linH <mr.linh1090@gmail.com>
2020-03-14 22:36:11 +0700

    #8 Make Admin Panel in Laravel 5.6

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function login(Request $request) {
+        if ($request->isMethod('post')) {
+            $data = $request->input();
+            if (Auth::attempt([ 'email'=>$data['email'], 'password'=> $data['password'], 'admin' =>'1' ])) {
+                return redirect('/admin/dashboard');
+            } 
+            // else {
+            //     echo 'failed';die;
+            // }
+        }
+        return view('admin.admin_login');
+    }
+
+    public function dashboard() {
+        return view('admin.dashboard');
+    }
+}
